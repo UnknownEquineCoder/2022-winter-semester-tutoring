77eba573ac60598d3ace017520e05c3a68496584
UnknownEquineCoder <antoninorossidev@gmail.com>
2022-10-14 00:34:04 +0200

    Added fizzbuzz exercise

---
+++ Lectures/lecture_01/exs/ex_04.py
@@ -0,0 +1,26 @@
+"""
+## Fizzbuzz
+
+This is a typical interview question.
+
+Consider the first N natural numbers (say, 1..100)
+
+If the number is divisible by 3, print fizz.
+If the number is divisible by 5, print buzz.
+If the number is divisible by 3 and 5, print fizzbuzz.
+Otherwise, print the number itself.
+
+### Variations
+
+#### Intermediate:
+Extend the Fizzbuzz algorithm to accept a third number, chug, which will appear on multiples of 7.
+
+#### Advanced:
+You receive a list of arbitrary words-to-number combinations.
+Dynamically implement a version of Fizzbuzz that works with that list (without knowing its contents).
+
+#### Crazy:
+You make a request on the web for a list of numbers and words.
+You parse them in the proper format and use multiprocessing to speed up your process.
+Use at least 5 subprocesses to analyze the output from 1 to 1000000.
+"""
